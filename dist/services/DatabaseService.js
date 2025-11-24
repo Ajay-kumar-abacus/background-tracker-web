8f258494870adb5c4f4f42e6e359cdb9cc5eed9d
Ajaya Behura <ajay@basiq360.in>
2025-11-24 21:09:05 +0530

    chnages

--- dist/services/DatabaseService.js
+++ dist/services/DatabaseService.js
@@ -14,83 +14,198 @@ var _a, _b, _c, _d;
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Location } from '@angular/common';
-import { Router, ActivatedRoute } from '@angular/router';
 import { retry } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
 import { MAP_LIBRARY_CONFIG } from '../map/map-config.token';
+import { ErrorService } from './error.service';
 const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
 const EXCEL_EXTENSION = '.xlsx';
 let DatabaseService = class DatabaseService {
-    constructor(http, router, location, route, config) {
+    constructor(http, _errService, location, router, route, config) {
         this.http = http;
-        this.router = router;
+        this._errService = _errService;
         this.location = location;
+        this.router = router;
         this.route = route;
         this.config = config;
+        // 🔥 REPLACED HARD-CODED HOPSER LINKS
+        this.dbUrl = this.config.apiBaseUrl;
+        this.uploadUrl = this.config.imageBaseUrl || '';
+        this.downloadUrl = (this.config.imageBaseUrl || '') + 'Download_excel/';
+        // everything else same
         this.header = new HttpHeaders();
+        this.myProduct = {};
+        this.peraluser = {};
+        this.detail = {};
+        this.counterArray = {};
+        this.drArray = [];
+        this.InfluenceArray = [];
+        this.leadArray = [];
+        this.counterArray1 = {};
+        this.orderFilterPrimary = {};
+        this.orderFilterSecondary = {};
+        this.dealerListSearch = {};
+        this.directDealerListSearch = {};
+        this.distributorListSearch = {};
+        this.login_data = {};
+        this.filteredData = {};
+        this.datauser = {};
+        this.challans = [];
+        this.pageLimit = 50;
+        this.notifications = [];
+        this.all_notifications = [];
+        this.offFlag = false;
+        this.offNotifiy = false;
+        this.st_user = (this.config.getUserData ? this.config.getUserData() : {}) || {};
     }
-    // -------------------- BASE URL HELPERS ------------------------- //
-    get baseUrl() {
-        return this.config.apiBaseUrl;
+    ngOnInit() { }
+    setData(data) {
+        this.filteredData = data;
     }
-    get uploadUrl() {
-        return this.config.imageBaseUrl;
+    getData() {
+        return this.filteredData;
     }
+    // 🔥 TOKEN FROM CONFIG INSTEAD OF LOCAL STORAGE
     get token() {
-        return this.config.getToken ? this.config.getToken() || '' : '';
+        return this.config.getToken ? this.config.getToken() : '';
     }
-    jsonHeaders() {
-        return new HttpHeaders()
-            .set('Content-Type', 'application/json')
-            .set('Token', 'Bearer ' + this.token);
+    auth_rqust(data, fn) {
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this.http.post(this.dbUrl + fn, JSON.stringify(data), { headers });
     }
-    formHeaders() {
-        return new HttpHeaders().set('Token', 'Bearer ' + this.token);
+    fetchData(data, fn) {
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this.http.post(this.dbUrl + fn, JSON.stringify(data), { headers });
     }
-    // ---------------------- BASIC REQUESTS ------------------------- //
-    getRequest(endpoint, data) {
-        return this.http.post(this.baseUrl + endpoint, JSON.stringify(data || {}), { headers: this.jsonHeaders() });
+    LogInCheck(username, password) {
+        this.data = { username, password };
+        return this.http.post(this.dbUrl + "/login/submitnew/", JSON.stringify(this.data), { headers: new HttpHeaders() });
     }
-    post_rqst(endpoint, data) {
-        return this.http.post(this.baseUrl + endpoint, JSON.stringify(data), { headers: this.jsonHeaders() });
-    }
-    uploadFile(endpoint, formData) {
-        return this.http.post(this.uploadUrl + endpoint, formData, {
-            headers: this.formHeaders()
-        });
-    }
-    // ----------------------- EXCEL EXPORT --------------------------- //
-    exportAsExcelFile(json, fileName) {
+    exportAsExcelFile(json, excelFileName) {
         const worksheet = XLSX.utils.json_to_sheet(json);
-        const workbook = {
-            Sheets: { data: worksheet },
-            SheetNames: ['data']
-        };
-        const excelBuffer = XLSX.write(workbook, {
-            bookType: 'xlsx',
-            type: 'array'
-        });
-        this.saveAsExcelFile(excelBuffer, fileName);
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, excelFileName);
     }
     saveAsExcelFile(buffer, fileName) {
         const data = new Blob([buffer], { type: EXCEL_TYPE });
         FileSaver.saveAs(data, fileName + EXCEL_EXTENSION);
     }
-    // -------------------- NOTIFICATION EXAMPLE ----------------------- //
-    getNotifications() {
-        return this.http
-            .post(this.baseUrl + 'stockdata/getNotification', JSON.stringify({}), { headers: this.jsonHeaders() })
-            .pipe(retry(3));
+    upload_image(val, fn_name) {
+        return this.http.post(this.uploadUrl + fn_name, val, {
+            headers: new HttpHeaders().set('Token', 'Bearer ' + this.token)
+        });
     }
-    // --------------------- SESSION HELPERS --------------------------- //
+    FileData(request_data, fn) {
+        let headers = new HttpHeaders().set('Token', 'Bearer ' + this.token);
+        return this.http.post(this.dbUrl + fn, request_data, { headers });
+    }
+    extractData(res) {
+        const body = res;
+        return body || {};
+    }
+    get_rqst2(request_data, fn) {
+        let headers = new HttpHeaders()
+            .set('Content-Type', 'application/json')
+            .set('Token', 'Bearer ' + this.token);
+        return this.http.post(this.dbUrl + fn, JSON.stringify(request_data), { headers });
+    }
+    set_fn(val) {
+        this.share_data = val;
+    }
+    get_fn() {
+        return this.share_data;
+    }
+    chek_seission() {
+        this.datauser = this.config.getUserData ? this.config.getUserData() : {};
+        if (this.datauser && this.datauser.id) {
+            return true;
+        }
+        else {
+            this.router.navigate([''], { queryParams: { returnUrl: this.router.url } });
+            return false;
+        }
+    }
     goBack() {
-        this.location.back();
+        window.history.back();
+    }
+    noificaton_rqst() {
+        var _a;
+        let headers = new HttpHeaders()
+            .set('Content-Type', 'application/json')
+            .set('Token', 'Bearer ' + this.token);
+        return this.http.post(this.dbUrl + 'stockdata/getNotification', JSON.stringify({ login_id: (_a = this.datauser) === null || _a === void 0 ? void 0 : _a.id }), { headers }).pipe(retry(3));
+    }
+    noificaton() {
+        this.noificaton_rqst().subscribe(d => {
+            this.all_notifications = d.notifications;
+            if (d.notify.length > 0 && !this.offNotifiy) {
+                this.offFlag = false;
+                this.notifications = d.notify;
+                this.sendNotify(0);
+            }
+        });
+    }
+    post_rqst(request_data, fn) {
+        let headers = new HttpHeaders()
+            .set('Content-Type', 'application/json')
+            .set('Token', 'Bearer ' + this.token);
+        return this.http.post(this.dbUrl + fn, JSON.stringify(request_data), { headers });
+    }
+    sendNotify(index) {
+        if (this.offFlag)
+            return;
+        var e = this.notifications[index];
+        if (!e)
+            return;
+        const title = e.title;
+        let options = {
+            body: e.message,
+            icon: 'favicon.ico'
+        };
+    }
+    numeric_Number(event) {
+        const pattern = /[0-9\+\-\ ]/;
+        let inputChar = String.fromCharCode(event.charCode);
+        if (event.keyCode != 8 && !pattern.test(inputChar)) {
+            event.preventDefault();
+        }
+    }
+    count_list() {
+        this.st_user = this.config.getUserData ? this.config.getUserData() : {};
+        if (this.st_user.data) {
+            this.login_data = this.st_user.data;
+        }
+        this.post_rqst({}, "Left_Navigation/left_navigation_counter").subscribe(r => {
+            if (r) {
+                this.counterArray = r['data'];
+            }
+        });
+    }
+    influencer_netwrk() {
+        var _a, _b;
+        this.st_user = this.config.getUserData ? this.config.getUserData() : {};
+        this.post_rqst({ user_id: (_a = this.st_user) === null || _a === void 0 ? void 0 : _a.id, user_type: (_b = this.st_user) === null || _b === void 0 ? void 0 : _b.type }, "Influencer/influencerMasterList").subscribe(r => {
+            if (r) {
+                this.InfluenceArray = r['result'] || r['modules'];
+            }
+        });
+    }
+    dr_list() {
+        var _a, _b;
+        this.st_user = this.config.getUserData ? this.config.getUserData() : {};
+        this.post_rqst({ user_id: (_a = this.st_user) === null || _a === void 0 ? void 0 : _a.id, user_type: (_b = this.st_user) === null || _b === void 0 ? void 0 : _b.type }, "CustomerNetwork/distributionNetworkModule").subscribe(r => {
+            if (r) {
+                this.drArray = r['modules'];
+            }
+        });
     }
 };
 DatabaseService = __decorate([
-    Injectable(),
-    __param(4, Inject(MAP_LIBRARY_CONFIG)),
-    __metadata("design:paramtypes", [typeof (_a = typeof HttpClient !== "undefined" && HttpClient) === "function" ? _a : Object, typeof (_b = typeof Router !== "undefined" && Router) === "function" ? _b : Object, typeof (_c = typeof Location !== "undefined" && Location) === "function" ? _c : Object, typeof (_d = typeof ActivatedRoute !== "undefined" && ActivatedRoute) === "function" ? _d : Object, Object])
+    Injectable({ providedIn: 'root' }),
+    __param(5, Inject(MAP_LIBRARY_CONFIG)),
+    __metadata("design:paramtypes", [typeof (_a = typeof HttpClient !== "undefined" && HttpClient) === "function" ? _a : Object, ErrorService, typeof (_b = typeof Location !== "undefined" && Location) === "function" ? _b : Object, typeof (_c = typeof Router !== "undefined" && Router) === "function" ? _c : Object, typeof (_d = typeof ActivatedRoute !== "undefined" && ActivatedRoute) === "function" ? _d : Object, Object])
 ], DatabaseService);
 export { DatabaseService };
--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -1,113 +1,252 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable, OnInit, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Location } from '@angular/common';
-import { Router, ActivatedRoute } from '@angular/router';
 import { retry } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
 
 import { MAP_LIBRARY_CONFIG } from '../map/map-config.token';
 import { MapLibraryConfig } from '../map/map.config';
+import { ErrorService } from './error.service';
 
-const EXCEL_TYPE =
-  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
 const EXCEL_EXTENSION = '.xlsx';
 
-@Injectable()
-export class DatabaseService {
-  header: HttpHeaders = new HttpHeaders();
+@Injectable({ providedIn: 'root' })
+export class DatabaseService implements OnInit {
 
-  constructor(
-    private http: HttpClient,
-    private router: Router,
-    private location: Location,
-    private route: ActivatedRoute,
-    @Inject(MAP_LIBRARY_CONFIG) private config: MapLibraryConfig
-  ) {}
+    constructor(
+        public http: HttpClient,
+        private _errService: ErrorService,
+        public location: Location,
+        private router: Router,
+        public route: ActivatedRoute,
 
-  // -------------------- BASE URL HELPERS ------------------------- //
+        @Inject(MAP_LIBRARY_CONFIG) private config: MapLibraryConfig
+    ) {
+        this.st_user = (this.config.getUserData ? this.config.getUserData() : {}) || {};
+    }
 
-  private get baseUrl(): string {
-    return this.config.apiBaseUrl;
-  }
+    // 🔥 REPLACED HARD-CODED HOPSER LINKS
+    dbUrl = this.config.apiBaseUrl;
+    uploadUrl = this.config.imageBaseUrl || '';
+    downloadUrl = (this.config.imageBaseUrl || '') + 'Download_excel/';
 
-  private get uploadUrl(): string {
-    return this.config.imageBaseUrl;
-  }
+    // everything else same
+    header: any = new HttpHeaders();
+    data: any;
+    myProduct: any = {};
+    peraluser: any = {};
+    tmp;
+    detail: any = {};
+    counterArray: any = {};
+    drArray: any = [];
+    InfluenceArray: any = [];
+    leadArray: any = [];
+    counterArray1: any = {};
+    st_user: any
+    orderFilterPrimary: any = {}
+    orderFilterSecondary: any = {}
+    dealerListSearch: any = {}
+    directDealerListSearch: any = {}
+    distributorListSearch: any = {}
+    login_data: any = {};
+    filteredData: any = {}
+    datauser: any = {};
+    loading: any;
+    customer_name: any;
+    franchise_name: any;
+    franchise_id;
+    franchise_location;
+    challans: any = [];
+    currentUserID: any;
+    pageLimit = 50;
 
-  private get token(): string {
-    return this.config.getToken ? this.config.getToken() || '' : '';
-  }
+    ngOnInit() { }
 
-  private jsonHeaders(): HttpHeaders {
-    return new HttpHeaders()
-      .set('Content-Type', 'application/json')
-      .set('Token', 'Bearer ' + this.token);
-  }
+    setData(data) {
+        this.filteredData = data;
+    }
 
-  private formHeaders(): HttpHeaders {
-    return new HttpHeaders().set('Token', 'Bearer ' + this.token);
-  }
+    getData() {
+        return this.filteredData;
+    }
 
-  // ---------------------- BASIC REQUESTS ------------------------- //
+    // 🔥 TOKEN FROM CONFIG INSTEAD OF LOCAL STORAGE
+    private get token() {
+        return this.config.getToken ? this.config.getToken() : '';
+    }
 
-  getRequest(endpoint: string, data?: any) {
-    return this.http.post(
-      this.baseUrl + endpoint,
-      JSON.stringify(data || {}),
-      { headers: this.jsonHeaders() }
-    );
-  }
+    auth_rqust(data: any, fn: any) {
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this.http.post(this.dbUrl + fn, JSON.stringify(data), { headers });
+    }
 
-  post_rqst(endpoint: string, data: any) {
-    return this.http.post(
-      this.baseUrl + endpoint,
-      JSON.stringify(data),
-      { headers: this.jsonHeaders() }
-    );
-  }
+    fetchData(data: any, fn: any) {
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this.http.post(this.dbUrl + fn, JSON.stringify(data), { headers });
+    }
 
-  uploadFile(endpoint: string, formData: any) {
-    return this.http.post(this.uploadUrl + endpoint, formData, {
-      headers: this.formHeaders()
-    });
-  }
+    LogInCheck(username, password) {
+        this.data = { username, password };
+        return this.http.post(this.dbUrl + "/login/submitnew/", JSON.stringify(this.data),
+            { headers: new HttpHeaders() });
+    }
 
-  // ----------------------- EXCEL EXPORT --------------------------- //
+    public exportAsExcelFile(json: any[], excelFileName: string): void {
+        const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+        const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, excelFileName);
+    }
 
-  exportAsExcelFile(json: any[], fileName: string): void {
-    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
-    const workbook: XLSX.WorkBook = {
-      Sheets: { data: worksheet },
-      SheetNames: ['data']
-    };
-    const excelBuffer: any = XLSX.write(workbook, {
-      bookType: 'xlsx',
-      type: 'array'
-    });
-    this.saveAsExcelFile(excelBuffer, fileName);
-  }
+    private saveAsExcelFile(buffer: any, fileName: string): void {
+        const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+        FileSaver.saveAs(data, fileName + EXCEL_EXTENSION);
+    }
 
-  private saveAsExcelFile(buffer: any, fileName: string): void {
-    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
-    FileSaver.saveAs(data, fileName + EXCEL_EXTENSION);
-  }
+    upload_image(val, fn_name) {
+        return this.http.post(this.uploadUrl + fn_name, val, {
+            headers: new HttpHeaders().set('Token', 'Bearer ' + this.token)
+        });
+    }
 
-  // -------------------- NOTIFICATION EXAMPLE ----------------------- //
+    FileData(request_data: any, fn: any) {
+        let headers = new HttpHeaders().set('Token', 'Bearer ' + this.token);
+        return this.http.post(this.dbUrl + fn, request_data, { headers });
+    }
 
-  getNotifications() {
-    return this.http
-      .post(
-        this.baseUrl + 'stockdata/getNotification',
-        JSON.stringify({}),
-        { headers: this.jsonHeaders() }
-      )
-      .pipe(retry(3));
-  }
+    private extractData(res: Response) {
+        const body = res;
+        return body || {};
+    }
 
-  // --------------------- SESSION HELPERS --------------------------- //
+    get_rqst2(request_data: any, fn: any): any {
+        let headers = new HttpHeaders()
+            .set('Content-Type', 'application/json')
+            .set('Token', 'Bearer ' + this.token);
 
-  goBack() {
-    this.location.back();
-  }
+        return this.http.post(this.dbUrl + fn, JSON.stringify(request_data), { headers });
+    }
+
+    public share_data: any;
+
+    set_fn(val: any) {
+        this.share_data = val;
+    }
+
+    get_fn() {
+        return this.share_data;
+    }
+
+    chek_seission() {
+        this.datauser = this.config.getUserData ? this.config.getUserData() : {};
+
+        if (this.datauser && this.datauser.id) {
+            return true;
+        } else {
+            this.router.navigate([''], { queryParams: { returnUrl: this.router.url } });
+            return false;
+        }
+    }
+
+    goBack() {
+        window.history.back();
+    }
+
+    noificaton_rqst(): any {
+        let headers = new HttpHeaders()
+            .set('Content-Type', 'application/json')
+            .set('Token', 'Bearer ' + this.token);
+
+        return this.http.post(this.dbUrl + 'stockdata/getNotification',
+            JSON.stringify({ login_id: this.datauser?.id }),
+            { headers }).pipe(retry(3));
+    }
+
+    notifications: any = [];
+    all_notifications: any = [];
+
+    noificaton() {
+        this.noificaton_rqst().subscribe(d => {
+            this.all_notifications = d.notifications;
+            if (d.notify.length > 0 && !this.offNotifiy) {
+                this.offFlag = false;
+                this.notifications = d.notify;
+                this.sendNotify(0);
+            }
+        });
+    }
+
+    post_rqst(request_data: any, fn: any): any {
+        let headers = new HttpHeaders()
+            .set('Content-Type', 'application/json')
+            .set('Token', 'Bearer ' + this.token);
+
+        return this.http.post(this.dbUrl + fn, JSON.stringify(request_data), { headers });
+    }
+
+    offFlag: any = false;
+    offNotifiy: any = false;
+
+    sendNotify(index) {
+        if (this.offFlag) return;
+        var e = this.notifications[index];
+        if (!e) return;
+
+        const title = e.title;
+        let options = {
+            body: e.message,
+            icon: 'favicon.ico'
+        };
+    }
+
+    numeric_Number(event: any) {
+        const pattern = /[0-9\+\-\ ]/;
+        let inputChar = String.fromCharCode(event.charCode);
+        if (event.keyCode != 8 && !pattern.test(inputChar)) {
+            event.preventDefault();
+        }
+    }
+
+    count_list() {
+        this.st_user = this.config.getUserData ? this.config.getUserData() : {};
+
+        if (this.st_user.data) {
+            this.login_data = this.st_user.data;
+        }
+
+        this.post_rqst({}, "Left_Navigation/left_navigation_counter").subscribe(r => {
+            if (r) {
+                this.counterArray = r['data'];
+            }
+        });
+    }
+
+    influencer_netwrk() {
+        this.st_user = this.config.getUserData ? this.config.getUserData() : {};
+
+        this.post_rqst(
+            { user_id: this.st_user?.id, user_type: this.st_user?.type },
+            "Influencer/influencerMasterList"
+        ).subscribe(r => {
+            if (r) {
+                this.InfluenceArray = r['result'] || r['modules'];
+            }
+        });
+    }
+
+    dr_list() {
+        this.st_user = this.config.getUserData ? this.config.getUserData() : {};
+
+        this.post_rqst(
+            { user_id: this.st_user?.id, user_type: this.st_user?.type },
+            "CustomerNetwork/distributionNetworkModule"
+        ).subscribe(r => {
+            if (r) {
+                this.drArray = r['modules'];
+            }
+        });
+    }
 }
